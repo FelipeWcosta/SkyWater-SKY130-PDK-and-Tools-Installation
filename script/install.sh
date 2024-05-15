9c7c8987d73abfdeee9497ed24f311d64c5be25d
Felipe Costa <costaf138@gmail.com>
2024-05-15 11:27:30 -0300

    fixing erros

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo "------------------------------------------------------------------ --------------"
-echo "First of all, we will fix some dependencies that may cause a bad working on tools"
-echo "------------------------------------------------------------------ --------------"
+echo "---------------------------------------------------------------------------------------"
+echo "---First of all, we will fix some dependencies that may cause a bad working on tools---"
+echo "---------------------------------------------------------------------------------------"
