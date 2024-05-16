2ce8dd1d1eea926fa2cf93627e37229c3f2bb6b5
Felipe Costa <costaf138@gmail.com>
2024-05-16 11:38:06 -0300

    changing script.sh

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# --------------------------------------------------
+# Script     : Installation of SkyWater PDK 130nm and VSLI tools like xschem, magic, ngspice...
+# Description: Installation script
+# Version    : 0.1
+# Author     : Felipe W. Costa <costaf138@gmail.com>
+# Date       : 16/05/2024
+# License    : GNU/GPL v3.0
+# --------------------------------------------------
+# Use        : ~/SkyWater-SKY130-PDK-and-Tools-Installation/script/install.sh
+# --------------------------------------------------
 
-echo "---------------------------------------------------------------------------------------"
-echo "---First of all, we will fix some dependencies that may cause a bad working on tools---"
-echo "---------------------------------------------------------------------------------------"
